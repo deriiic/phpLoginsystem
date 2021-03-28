e85d7e3847f41f51c1a38a5437260c8be21c3cb1
Amar <amar.deriiic@gmail.com>
2021-03-28 10:47:02 +0200

    First commit

--- classes/Db.php
+++ classes/Db.php
@@ -11,7 +11,7 @@ class Db
 
     public function __construct()
     {
-        $this->connection = new mysqli($_HOST = 'localhost', $_USER = 'root', $_PASS = 'M3ilPh0n3t!?', $_DB = 'loginsystem');
+        $this->connection = new mysqli($_HOST = 'localhost', $_USER = 'root', $_PASS = '', $_DB = 'loginsystem');
 
         if ($this->connection->connect_error) {
             $this->error('Could not connect to MySQL - ' . $this->connection->connect_error);
